f6e7c2a46b48eb475c7bfe80e878855779a2463c
MiguelOliveira10260 <miguel777oliveira@hotmail.com>
2023-11-04 23:48:41 +0000

    First upload

---
+++ app/src/main/java/com/example/calcularimc/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.calcularimc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textResultado;
+    private EditText editPeso;
+    private EditText editAltura;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textResultado = findViewById(R.id.textResultado);
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+    }
+
+    public void calcularImc(View view) {
+        double peso = Double.parseDouble(editPeso.getText().toString());
+        double altura = Double.parseDouble(editAltura.getText().toString());
+        double resultado = peso / (altura * altura);
+
+        //se
+        if ( resultado <19 ) {
+
+            textResultado.setText("Abaixo do Peso");
+
+        } else if (resultado <= 19 || resultado <25){
+
+            textResultado.setText("Peso normal");
+
+        } else if (resultado <= 25 || resultado <30){
+
+            textResultado.setText("Excesso de Peso");
+
+        } else if (resultado <= 30 || resultado <35){
+
+            textResultado.setText("Obesidade tipo I");
+
+        } else if (resultado <= 35 || resultado <40){
+
+            textResultado.setText("Obesidade tipo II");
+
+        } else if (resultado >= 40){
+
+            textResultado.setText("Obesidade tipo III");
+
+        }
+    }
+}
